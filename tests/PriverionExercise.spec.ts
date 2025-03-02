c75b34d7af0a280d9a2c13d12f7bc5341a25cb04
Jeison <jeison.herrera1592@gmail.com>
2025-03-01 22:31:11 -0500

    Search ROPA module and access

--- tests/PriverionExercise.spec.ts
+++ tests/PriverionExercise.spec.ts
@@ -3,11 +3,11 @@ import { test, expect } from '@playwright/test';
 test('Priverion_Exercise',async({ page }) => {
 
 
-//Reajust screen resolution 
+//Resize screen resolution 
 
     await page.setViewportSize({width:1024,height:768})
 
-//Login in PriverionPlatform
+//Login in PriverionPlatform - Fill in email and password to access
 
     await page.goto('https://hwazicsshi-testing.priverion.dev/login')
     await page.getByRole('textbox',{name:'E-Mail Address'}).fill('jeison.herrera1592@gmail.com')
@@ -15,9 +15,20 @@ test('Priverion_Exercise',async({ page }) => {
     await page.getByRole('textbox',{name:'Password'}).fill('Jeisonpruebapriverion12345@')
     await page.getByRole('button',{name:'Login'}).click()
 
+//Assertion Home page - Task Monitor Element
+    await expect(page.locator('.dashboard-title[data-cy="subtitle-task_monitor"]')).toBeVisible()
+
+//Go to Record of Processing Activities Module
+
+    await page.locator('.main-header__create-button').click()
+    await page.getByRole('link',{name:'Record of Processing Activities'}).waitFor()
+    await page.getByRole('link',{name:'Record of Processing Activities'}).click()
+    
+//Assertion Record of Processing Activities page - Save Button
+    await expect(page.getByRole('button',{name:'Save'})).toBeVisible()
+
     await page.pause()
 
-    
-    
+
 
 })
